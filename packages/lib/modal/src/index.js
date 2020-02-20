adfe7045e22901e1bb12644798d3d57bf11b048f
Leandro C. Silva <leandro.cesar@novahaus.com.br>
2020-02-20 15:02:56 -0300

    Add accessibility on modal packages, fix class no-scroll on html element

--- packages/lib/modal/src/index.js
+++ packages/lib/modal/src/index.js
@@ -1,12 +1,12 @@
 import selector, { all } from '../../../utils/selector';
 
 const modalCount = [];
+const focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
 let lastOpen = null;
 
 const defaultOptions = {
   hashHistory: true,
   disableScroll: true,
-  disableEvents: false,
 };
 
 function modal(elm, opts) {
@@ -14,34 +14,62 @@ function modal(elm, opts) {
   const nameModal = ctx.getAttribute('id');
   const closebutton = Array.from(ctx.querySelectorAll('[halley-modal-close]'));
   const openButtons = Array.from(document.querySelectorAll(`[href="#${nameModal}"]`));
+  const focusableElements = Array.from(ctx.querySelectorAll(focusableElementsString));
   const options = Object.assign({}, defaultOptions, opts);
   const structureData = { elm: ctx, nameModal, toggleModal, open, close };
+  const tabStop = { first: focusableElements[0], last: focusableElements[focusableElements.length - 1] };
   let scrollHeight = 0;
+
+  function changeTabIndex(i) {
+    focusableElements.forEach((focus) => focus.setAttribute('tabindex', i));
+  }
+
+  function setupTabFocus() {
+    ctx.addEventListener('keydown', trapTabKey);
+  }
+
+  function trapTabKey(event) {
+    if (event.keyCode === 27) close();
+    if (event.keyCode === 9) {
+      if ((event.shiftKey) && (document.activeElement === tabStop.first)) {
+        event.preventDefault();
+        tabStop.last.focus();
+      } else if (document.activeElement === tabStop.last) {
+        event.preventDefault();
+        tabStop.first.focus();
+      }
+    }
+  }
 
   function open(event) {
     if (event && options.disableEvents) event.preventDefault();
+    ctx.setAttribute('aria-hidden', false);
+    changeTabIndex(0);
     ctx.classList.add('active');
     modalCount.push(nameModal);
     openButtons.forEach((btn) => btn.classList.add('active'));
 
-    if (options.disableScroll) document.body.classList.add('no-scroll');
-    if (options.hashHistory) window.location.hash = `#${nameModal}`;
+    if (options.disableScroll) document.documentElement.classList.add('no-scroll');
     if (options.onOpen) options.onOpen.call(ctx, event);
+    if (options.hashHistory) window.location.hash = `#${nameModal}`;
     if (!options.preserve && lastOpen) {
       lastOpen.close();
     }
 
+    setupTabFocus();
     lastOpen = structureData;
   }
 
   function close(event) {
-    if (event && options.disableEvents) event.preventDefault();
+    event.preventDefault();
+    ctx.setAttribute('aria-hidden', true);
+    changeTabIndex(-1);
     ctx.classList.remove('active');
     modalCount.pop();
 
     if (modalCount.length === 0) {
       window.location.hash = '';
-      if (options.disableScroll) document.body.classList.remove('no-scroll');
+      if (options.disableScroll) document.documentElement.classList.remove('no-scroll');
     } else if (options.hashHistory) {
       window.location.hash = `#${modalCount[modalCount.length - 1]}`;
     }
@@ -53,16 +81,24 @@ function modal(elm, opts) {
   }
 
   function toggleModal(event) {
-    if (event && options.disableEvents) event.preventDefault();
+    event.preventDefault();
 
     if (ctx.classList.contains('active')) {
       close(event);
-      window.scrollBy(0, scrollHeight);
-      scrollHeight = 0;
+      setTimeout(() => {
+        window.scrollBy(0, scrollHeight)
+        scrollHeight = 0;
+      }, 1)
     } else {
-      scrollHeight = window.scrollY;
       open(event);
+      scrollHeight = window.scrollY;
     }
+  }
+
+  function setupAccessibility() {
+    ctx.setAttribute('aria-hidden', true);
+    closebutton.forEach((bt) => bt.setAttribute('aria-label', 'Fechar'));
+    changeTabIndex(-1);
   }
 
   function setupListeners() {
@@ -72,6 +108,7 @@ function modal(elm, opts) {
 
   function init() {
     setupListeners();
+    setupAccessibility();
 
     if (window.location.hash === `#${nameModal}`) {
       open();
